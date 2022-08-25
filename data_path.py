aa79e5404907abef54edfd32b8c386bdac35a501
foz <foz>
2022-08-25 23:38:02 +0100

    fix data path

--- data_path.py
+++ data_path.py
@@ -5,4 +5,4 @@ Set this DATA_PATH variable to point to the downloaded dataset
 
 #DATA_PATH='/home/foz/leaf_data_new/'
 #DATA_PATH='/media/foz/moredata2/leaf_data/'
-DATA_PATH='/media/foz/tempdata/cotyledon_data'
+DATA_PATH='/media/foz/tempdata/cotyledon_data/'
